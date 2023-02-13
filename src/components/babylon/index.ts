670d6e9c83148587ca8c93389f9b5fcd53299257
wangqz <2809755637@qq.com>
2023-02-13 10:07:29 +0800

    init

--- src/components/babylon/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: wangqz
- * @Date: 2023-02-12
- * @LastEditTime: 2023-02-12
- * @Description: content
- */
-import Ball from "./Ball.vue";
-import Foot from "./Foot.vue";
-export { Ball, Foot };
